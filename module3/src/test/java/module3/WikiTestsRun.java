fce3834a79b5672ccc5bdf470e879cf8fe64194a
Kseniia Semenova <kseniia_semenova@epam.com>
2020-03-15 22:04:21 +0200

    refactor (make a parent pom for modules) + corrections the second module, task 2 (path for properties_out)

--- module3/src/test/java/module3/WikiTestsRun.java
+++ module3/src/test/java/module3/WikiTestsRun.java
@@ -7,7 +7,6 @@ import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.JUnitStories;
-import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.*;
 
@@ -22,9 +21,6 @@ public class WikiTestsRun extends JUnitStories {
         return new MostUsefulConfiguration()
                 .useStoryLoader(new LoadFromClasspath(this.getClass().getClassLoader()))
                 .useStoryReporterBuilder(new StoryReporterBuilder()
-                        .withDefaultFormats()
-                        .withFormats(Format.HTML, Format.CONSOLE)
-                        .withRelativeDirectory("jbehave-report")
                         .withFailureTrace(true)
                 );
     }
@@ -37,7 +33,6 @@ public class WikiTestsRun extends JUnitStories {
         return new InstanceStepsFactory(configuration(), stepFileList);
     }
 
-    @Override
     protected List<String> storyPaths() {
         return new StoryFinder().
                 findPaths(CodeLocations.codeLocationFromClass(
