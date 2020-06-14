bb898f04fc4df34659897e652bdeeeebc68c3831
kseniia_ss <kseniia.semenova@refinitive.com>
2020-06-14 23:44:41 +0300

    Add Browser prefix to story name

--- module3/src/test/java/module3/BrowserRunner.java
+++ module3/src/test/java/module3/BrowserRunner.java
@@ -1,9 +1,6 @@
 package module3;
 
 import io.qameta.allure.AllureLifecycle;
-import io.qameta.allure.FileSystemResultsWriter;
-import io.qameta.allure.aspects.AttachmentsAspects;
-import io.qameta.allure.aspects.StepsAspects;
 import io.qameta.allure.jbehave.AllureJbehave;
 import module3.enums.BrowserType;
 import module3.stepDefs.WikipediaPagesStepDef;
@@ -14,14 +11,13 @@ import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.parsers.MyStoryParser;
 import org.jbehave.core.reporters.CrossReference;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 import org.jbehave.core.steps.Steps;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -31,9 +27,11 @@ import static org.jbehave.core.reporters.Format.*;
 public class BrowserRunner extends ConfigurableEmbedder {
 
     private BrowserType browserType;
+    private AllureJbehave allureReporter;
 
-    public BrowserRunner(BrowserType type) {
+    public BrowserRunner(BrowserType type, AllureLifecycle lifecycle) {
         browserType = type;
+        allureReporter = new AllureJbehave(lifecycle);
     }
 
     @Override
@@ -50,21 +48,17 @@ public class BrowserRunner extends ConfigurableEmbedder {
 
     @Override
     public Configuration configuration() {
-        FileSystemResultsWriter results = new FileSystemResultsWriter(Paths.get("target/allure-results"));
-        final AllureLifecycle lifecycle = new AllureLifecycle(results);
-        StepsAspects.setLifecycle(lifecycle);
-        AttachmentsAspects.setLifecycle(lifecycle);
-
         return new MostUsefulConfiguration()
                 .useStoryLoader(new LoadFromClasspath(this.getClass().getClassLoader()))
+                .useStoryParser(new MyStoryParser(String.valueOf(this.browserType)))
                 .useStoryReporterBuilder(new StoryReporterBuilder()
-                        .withReporters(new AllureJbehave(lifecycle))
+                .withReporters(allureReporter)
 //                        .withCodeLocation(CodeLocations.codeLocationFromClass(this.getClass()))
-                        .withDefaultFormats()
-                        .withFormats(CONSOLE, TXT, HTML, XML)
-                        .withCrossReference(new CrossReference()))
-                //.useParameterConverters(parameterConverters);
-                ;
+                .withDefaultFormats()
+                .withFormats(CONSOLE, TXT, HTML, XML)
+                .withCrossReference(new CrossReference()))
+        //.useParameterConverters(parameterConverters);
+        ;
     }
 
     @Override
--- module3/src/test/java/module3/WikiTestsRun.java
+++ module3/src/test/java/module3/WikiTestsRun.java
@@ -1,5 +1,9 @@
 package module3;
 
+import io.qameta.allure.AllureLifecycle;
+import io.qameta.allure.FileSystemResultsWriter;
+import io.qameta.allure.aspects.AttachmentsAspects;
+import io.qameta.allure.aspects.StepsAspects;
 import module3.enums.BrowserType;
 import org.assertj.core.util.Lists;
 import org.jbehave.core.embedder.Embedder;
@@ -7,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.nio.file.Paths;
 import java.util.List;
 
 @RunWith(JUnit4.class)
@@ -17,9 +22,14 @@ public class WikiTestsRun {
     @Test
     public void runTests() {
 
+        FileSystemResultsWriter results = new FileSystemResultsWriter(Paths.get("target/allure-results"));
+        final AllureLifecycle lifecycle = new AllureLifecycle(results);
+        StepsAspects.setLifecycle(lifecycle);
+        AttachmentsAspects.setLifecycle(lifecycle);
+
         for (BrowserType browserType : browserList) {
             try {
-                BrowserRunner runner = new BrowserRunner(browserType);
+                BrowserRunner runner = new BrowserRunner(browserType, lifecycle);
                 runner.run();
             } catch (Embedder.RunningStoriesFailed e) {
                 // pass execution to next browser
